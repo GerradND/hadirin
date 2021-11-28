53f9dde3c05b738f03cd6430a0e1877b0dd92f52
Antonius Anggito Arissaputro <antoniusanggito@gmail.com>
2021-11-28 23:57:44 +0700

    Init static izin pages

--- perizinan/urls.py
+++ perizinan/urls.py
@@ -5,4 +5,7 @@ app_name = 'perizinan'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('add', views.add_izin, name='add_izin'),
+    path('validasi', views.validasi_izin, name='validasi_izin'),
+    path('validasi/detail/<int:id>', views.detail_izin, name='detail_izin'),
 ]
--- perizinan/views.py
+++ perizinan/views.py
@@ -2,3 +2,38 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'perizinan/index.html')
+
+def validasi_izin(request):
+    context = {
+        'daftar_izin': [
+            {'id': 1,
+            'staf': 'Agung',
+            'tanggal': '22/06/2021',
+            'keterangan': 'Capek kerja pak',
+            'status': 'pending'},
+            {'id': 2,
+            'staf': 'Bejo',
+            'tanggal': '22/06/2022',
+            'keterangan': 'Sama pak',
+            'status': 'pending'}
+            ]
+    }
+    return render(request, 'perizinan/validasi_izin.html', context)
+
+def detail_izin(request, id):
+    context = {
+        'izin': {
+            'id': 1,
+            'staf': 'Agung',
+            'tanggal': '22/06/2021',
+            'keterangan': 'Capek kerja pak',
+            'status': 'pending'
+            }
+    }
+    return render(request, 'perizinan/detail_izin.html', context)
+
+def add_izin(request):
+    return render(request, 'perizinan/add_izin.html')
+
+def add(request):
+    return render(request, 'perizinan/index.html')
