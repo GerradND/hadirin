cac8ec4d7aff73321310e2383242154890c4e676
Francis Wibisono <francis.wibisono@ui.ac.id>
2021-11-28 23:37:25 +0700

    feat: added profile layout

--- main/urls.py
+++ main/urls.py
@@ -6,4 +6,5 @@ app_name = 'main'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('profil', views.profil, name='profil')
 ]
--- main/views.py
+++ main/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'main/home.html')
+
+def profil(request):
+    return render(request, 'main/profil.html')
