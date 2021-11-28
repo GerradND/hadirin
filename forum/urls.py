48fe11e4f5a3d7fe60daa09e96e42c0fd3d7862c
hanifanggawi <hanifanggawi45@gmail.com>
2021-11-28 23:02:46 +0700

    added edit thread page (static)

--- forum/urls.py
+++ forum/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('add/', views.add_thread, name='add_thread'),
     path('1/', views.thread_detail, name='detail'),
-    path('1/edit', views.edit_thread, name='edit_thread'),
+    path('1/edit/', views.edit_thread, name='edit_thread'),
 ]
--- forum/views.py
+++ forum/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
 
+thread = {
+    'user' : 'Budi Budiman',
+    'title' : "Lorem ipsum dolor sit amet conquistador",
+    'content' : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    'replies' : [
+        {
+            'user' : 'Susan Susanti', 
+            'content' : 'Saya Setuju dengan pendapat anda' 
+        },
+        {
+            'user' : 'Rudi Rudiman', 
+            'content' : 'Saya tidak etuju' 
+        }
+    ]
+}
+
+threads = [thread, thread]
+
 # Create your views here.
 def home(request):
     return render(request, 'forum/index.html')
@@ -9,3 +27,6 @@ def thread_detail(request):
 
 def add_thread(request):
     return render(request, 'forum/add_thread.html')
+
+def edit_thread(request):
+    return render(request, 'forum/edit_thread.html', {'thread' : thread})
