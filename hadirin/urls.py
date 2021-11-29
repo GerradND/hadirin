fac8556162b6e35fab67d9923295b6b5471ca2ff
GerradND <Gerrad1106@gmail.com>
2021-11-29 12:20:42 +0700

    add urls.py

--- hadirin/urls.py
+++ hadirin/urls.py
@@ -19,6 +19,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-    path('pengumuman', include('pengumuman.urls')),
+    path('pengumuman/', include('pengumuman.urls')),
     path('forum/', include('forum.urls')),
 ]
