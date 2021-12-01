093ece26f4b8dfb0d77ac9aa94b237a1818a3ecf
Teofanus Gary <garyteofanus@gmail.com>
2021-12-01 12:55:27 +0700

    Change template referencing in views

--- role_divisi/views.py
+++ role_divisi/views.py
@@ -5,20 +5,22 @@ from django.urls import reverse_lazy
 
 class IndexView(ListView):
     model = RoleDivisi
+    template_name = "role_divisi/index.html"
 
 
 class AddRoleDivisiView(CreateView):
     model = RoleDivisi
     fields = '__all__'
-    template_name_suffix = "_add_form"
+    template_name = "role_divisi/add_form.html"
 
 
 class EditRoleDivisiView(UpdateView):
     model = RoleDivisi
     fields = ['keterangan']
-    template_name_suffix = "_edit_form"
+    template_name = "role_divisi/edit_form.html"
 
 
 class DeleteRoleDivisiView(DeleteView):
     model = RoleDivisi
     success_url = reverse_lazy('role-divisi-list')
+    template_name = "role_divisi/confirm_delete.html"
