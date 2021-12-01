247a743e3161f92b4cef6afa6379d35bb284b34a
GerradND <Gerrad1106@gmail.com>
2021-12-01 21:13:11 +0700

    fix some writing errors

--- pengumuman/views.py
+++ pengumuman/views.py
@@ -1,23 +1,5 @@
 from django.shortcuts import render
 from .forms import PengumumanForm
-# Dummy Data
-thread = {
-    'user' : 'Budi Budiman',
-    'title' : "Lorem ipsum dolor sit amet conquistador",
-    'content' : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-    'replies' : [
-        {
-            'user' : 'Susan Susanti', 
-            'content' : 'Saya Setuju dengan pendapat anda' 
-        },
-        {
-            'user' : 'Rudi Rudiman', 
-            'content' : 'Saya tidak etuju' 
-        }
-    ]
-}
-
-threads = [thread, thread]
 
 # Create your views here.
 def pengumuman(request):
