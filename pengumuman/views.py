1c6342482f620f1094cb8b8db190ccd3199a37e0
GerradND <Gerrad1106@gmail.com>
2021-12-01 21:42:43 +0700

    add success notification

--- pengumuman/views.py
+++ pengumuman/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import redirect, render
 from .forms import PengumumanForm
 
 # Create your views here.
 def pengumuman(request):
-
     return render(request, 'pengumuman/pengumuman.html')
 
 def pengumuman_saya(request):
@@ -11,11 +11,20 @@ def pengumuman_saya(request):
 
 def buat_pengumuman(request):
     form = PengumumanForm()
+    if request.method == 'POST':
+        messages.success(request, 'Pengumuman berhasil dibuat!')
+        return redirect('pengumuman:pengumuman_saya')
     return render(request, 'pengumuman/buat_pengumuman.html', {'form' : form})
 
 def edit_pengumuman(request):
     form = PengumumanForm()
+    if request.method == 'POST':
+        messages.success(request, 'Pengumuman berhasil diubah!')
+        return redirect('pengumuman:pengumuman_saya')
     return render(request, 'pengumuman/edit_pengumuman.html', {'form' : form})
 
 def hapus_pengumuman(request):
+    if request.method == 'POST':
+        messages.success(request, 'Pengumuman berhasil dihapus!')
+        return redirect('pengumuman:pengumuman_saya')
     return render(request, 'pengumuman/hapus_pengumuman.html')
