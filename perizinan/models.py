727a9c9150b601ba99ba46a00c99ce93a9a47081
Antonius Anggito Arissaputro <antoniusanggito@gmail.com>
2021-12-03 11:09:13 +0700

    Init izin model

--- perizinan/models.py
+++ perizinan/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Izin(models.Model):
+    id = models.IntegerField(primary_key=True)
+    # staf = models.ForeignKey(User, on_delete=models.CASCADE)
+    staf = models.CharField(max_length=20)
+    tanggal = models.DateField()
+    keterangan = models.TextField()
+    status = models.CharField(max_length=10, default="Pending")
+
+    def __str__(self):
+        return f'{self.id}. {self.staf} {self.tanggal}'
