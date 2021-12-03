3652153087595e6643000be0bbd866d4a6ed14b0
Antonius Anggito Arissaputro <antoniusanggito@gmail.com>
2021-12-03 12:33:47 +0700

    Fix Izin model & IzinForm id

--- perizinan/models.py
+++ perizinan/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Izin(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     # staf = models.ForeignKey(User, on_delete=models.CASCADE)
     staf = models.CharField(max_length=20)
     tanggal = models.DateField()
