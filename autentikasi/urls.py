586204c38a4c6ea066ef7b5b155cdb48665cf32a
hanifanggawi <hanifanggawi45@gmail.com>
2021-12-04 11:13:02 +0700

    added simple login and logut views

--- autentikasi/urls.py
+++ autentikasi/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'autentikasi'
 
 urlpatterns = [
-    path('login', views.login, name='login'),
-    path('register', views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name = "autentikasi/login.html"), name='login'),
+    path('logout/', views.logout_view , name="logout"),
+    path('register/', views.register, name='register'),
 ] 
