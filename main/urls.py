5506159d20ec5e90cf7e7222c8f7991ba28ac0c5
Francis Wibisono <francis.wibisono@ui.ac.id>
2021-12-08 03:35:31 +0700

    fix: login

--- main/urls.py
+++ main/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'main'
 
 urlpatterns = [
-    path('', views.presensi, name='home'),
+    path('', views.presensi.as_view(), name='home'),
     path('profil', views.profil, name='profil'),
     path('profil/edit', views.profil_edit, name='profil_edit'),
     path('presensi', views.presensi.as_view(), name='presensi'),
