cd8211add308da72d22cbfccc1cce7113d911f88
Antonius Anggito Arissaputro <antoniusanggito@gmail.com>
2021-12-08 22:49:06 +0700

    Clean Izin code

--- perizinan/views.py
+++ perizinan/views.py
@@ -3,30 +3,34 @@ from .forms import IzinForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, render
-import datetime
+
 
 @login_required
 def index(request):
     return render(request, 'perizinan/index.html')
 
+
 @login_required
 def add_izin(request):
     form = IzinForm(request.POST)
-
     if request.method == 'POST':
         form = IzinForm(request.POST)
         if form.is_valid():
-            print(request.POST)
             izin = form.save(commit=False)
             izin.staf = request.user
             izin.save()
-        return redirect('perizinan:index')
+            messages.success(request, 'Izin berhasil diajukan ke supervisor')
+            return redirect('perizinan:index')
+        else:
+            messages.error(request, form.errors)
     return render(request, 'perizinan/add_izin.html', {'form': form})
+
 
 @login_required
 def validasi_izin(request):
-    daftar_izin = Izin.objects.all()
+    daftar_izin = Izin.objects.all().order_by('status')
     return render(request, 'perizinan/validasi_izin.html', {'daftar_izin': daftar_izin})
+
 
 @login_required
 def detail_izin(request, id):
