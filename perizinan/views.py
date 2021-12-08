e548cab0022d1a076bbcbeb1af34b6ce47bef6d0
Antonius Anggito Arissaputro <antoniusanggito@gmail.com>
2021-12-08 21:35:23 +0700

    Add login integration

--- perizinan/views.py
+++ perizinan/views.py
@@ -1,14 +1,15 @@
 from .models import Izin
 from .forms import IzinForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, render
 import datetime
 
-
+@login_required
 def index(request):
     return render(request, 'perizinan/index.html')
 
-
+@login_required
 def add_izin(request):
     form = IzinForm(request.POST)
 
@@ -22,12 +23,12 @@ def add_izin(request):
         return redirect('perizinan:index')
     return render(request, 'perizinan/add_izin.html', {'form': form})
 
-
+@login_required
 def validasi_izin(request):
     daftar_izin = Izin.objects.all()
     return render(request, 'perizinan/validasi_izin.html', {'daftar_izin': daftar_izin})
 
-
+@login_required
 def detail_izin(request, id):
     izin = get_object_or_404(Izin, pk=id)
     if request.method == 'POST':
